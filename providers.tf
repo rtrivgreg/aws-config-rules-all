49080a59e353f6c5975f42183ee4b07770c1e540
rtrivgreg <rtrivgreg@yahoo.com>
2026-07-19 19:09:04 -0400

    Add all AWS managed Config rules

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
   # No credentials here — TFC OIDC via TFC_AWS_PROVIDER_AUTH
   # and TFC_AWS_RUN_ROLE_ARN handles all authentication
 }
