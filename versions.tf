c5e31d0b00c88d33a3b969d4f5526d5aaec79653
rtrivgreg <rtrivgreg@yahoo.com>
2026-07-25 07:16:47 -0400

    Add Terraform Cloud configuration to versions.tf

--- versions.tf
+++ versions.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  cloud {
+    organization = "RSHL2136"
+    workspaces {
+      name = "config-rules-all"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
