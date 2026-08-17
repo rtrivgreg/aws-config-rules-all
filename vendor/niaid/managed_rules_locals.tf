b000bb9edbf37fcaf116d81a1aa975f46b44f1c6
Raymond Gregoire <rtrivgreg@yahoo.com>
2026-08-17 09:23:16 -0400

    Update input parameter for Aurora PostgreSQL rule

--- vendor/niaid/managed_rules_locals.tf
+++ vendor/niaid/managed_rules_locals.tf
@@ -6021,7 +6021,7 @@ locals {
 rds-global-cluster-aurora-postgresql-supported-version = {
       description          = "Checks if Amazon Aurora PostgreSQL global database clusters are running on a specified minimum supported engine version. The rule is NON_COMPLIANT if the database is running on an older version."
       identifier           = "RDS_GLOBAL_CLUSTER_AURORA_MYSQL_SUPPORTED_VERSION"
-      input_parameters     = var.minSupportedEngineVersion
+      input_parameters     = var.rds_global_cluster_aurora_postgresql_supported_version
       resource_types_scope = ["AWS::RDS::GlobalCluster"]
       severity             = "High"
     }
