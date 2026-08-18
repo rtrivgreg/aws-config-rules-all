4613a16e802532f61c926d633b21a221b0322cdf
Raymond Gregoire <rtrivgreg@yahoo.com>
2026-08-18 05:28:04 -0400

    Update identifier for Aurora PostgreSQL rule

--- vendor/niaid/managed_rules_locals.tf
+++ vendor/niaid/managed_rules_locals.tf
@@ -6020,7 +6020,7 @@ locals {
 # 20260817
   rds-global-cluster-aurora-postgresql-supported-version = {
       description          = "Checks if Amazon Aurora PostgreSQL global database clusters are running on a specified minimum supported engine version. The rule is NON_COMPLIANT if the database is running on an older version."
-      identifier           = "RDS_GLOBAL_CLUSTER_AURORA_MYSQL_SUPPORTED_VERSION"
+      identifier           = "RDS_GLOBAL_CLUSTER_AURORA_POSTGRESQL_SUPPORTED_VERSION"
       input_parameters     = var.rds_global_cluster_aurora_postgresql_supported_version
       resource_types_scope = ["AWS::RDS::GlobalCluster"]
       severity             = "High"
